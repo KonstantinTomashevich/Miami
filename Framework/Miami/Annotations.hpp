27c7c8afa2caf5661604b852de321dfdc0448eda
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-01-02 22:56:40 +0300

    Add readability annotations.

---
+++ Framework/Miami/Annotations.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// TODO: Think if this annotations really help with readability.
+#define free_call /* isn't protected by any std synchronization primitive */
+#define abstract /* abstract class marker */
+#define requirement(...) /* requirement for inheritors */
